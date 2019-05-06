73faafc6f9f257ffd8c35f68541a68107e58840f
Yurii Kolesnykov <root@yurikoles.com>
2019-05-06 18:38:54 +0300

    fix child vc, cleanup

--- AudioNotes/AudioNotes/AudioListViewController.swift
+++ AudioNotes/AudioNotes/AudioListViewController.swift
@@ -10,113 +10,107 @@ import UIKit
 import AVFoundation
 
 class AudioListViewController: UIViewController {
-    
-    
     @IBOutlet weak var addThemsOutlet: UIButton!
     @IBOutlet weak var audioCollection: UICollectionView!
-    
-    
-    
-    @IBAction func addThemesButton(_ sender: Any) {callingAlert()
-        
+
+    @IBAction func addThemesButton(_ sender: Any) {
+        callingAlert()
     }
-    
+
     let vc = ViewController()
-    let workerAudioFile = WorkerAudioFile();
-    
+    let workerAudioFile = WorkerAudioFile()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        addThemsOutlet.layer.cornerRadius = addThemsOutlet.frame.size.height/2
-        self.audioCollection.reloadData()
+        addThemsOutlet.layer.cornerRadius = addThemsOutlet.frame.size.height / 2
+        audioCollection.reloadData()
         audioCollection.delegate = self
         audioCollection.dataSource = self
-//        audioCollection.
-        }
-    
-    
- 
+        //        audioCollection.
+    }
 }
 
-
-extension AudioListViewController : UICollectionViewDelegate,UICollectionViewDataSource{
-
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
+extension AudioListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
         if section == 0 {
-            
             let count = Storage.shared.numberOfRecords
             return count
-        }else{
-            return 5}
-        
+        } else {
+            return 5
+        }
     }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
-        cell.audioCellLabel.text = String(indexPath.row+1)
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell",
+                                                      for: indexPath) as! CollectionViewCell
+        cell.audioCellLabel.text = String(indexPath.row + 1)
         cell.backgroundColor = UIColor.green
-        
 
         return cell
     }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+    func collectionView(_ collectionView: UICollectionView,
+                        didSelectItemAt indexPath: IndexPath) {
         print("tab \(indexPath.row+1)")
-        
-        let path = workerAudioFile.getDocumetnsDirector().appendingPathComponent("Records\(indexPath.row+1).m4a")
-        
-        do
-        {
+
+        let path = workerAudioFile.getDocumetnsDirector()
+            .appendingPathComponent("Records\(indexPath.row + 1).m4a")
+
+        do {
             workerAudioFile.soundPlayer = try AVAudioPlayer(contentsOf: path)
             workerAudioFile.soundPlayer.play()
-        }catch{
-            
+        } catch {
+
         }
     }
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-    
-        return Storage.shared.themes.count
-        
-        
-    }
-    
-  //  - MARK : Header section in collectionView
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
 
-        if let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "CollectionReusableView", for: indexPath)
-            as? CollectionReusableView{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return Storage.shared.themes.count
+    }
+
+    //  - MARK : Header section in collectionView
+    func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
+        if let sectionHeader = collectionView
+            .dequeueReusableSupplementaryView(ofKind: kind,
+                                              withReuseIdentifier: "CollectionReusableView",
+                                              for: indexPath) as? CollectionReusableView {
             sectionHeader.sectionHeaderlabel.text = Storage.shared.themes[indexPath.section] // заголовок секций
-//            sectionHeader.sectionHeaderlabel.text = "Section \(indexPath.section)"
+            //            sectionHeader.sectionHeaderlabel.text = "Section \(indexPath.section)"
             return sectionHeader
         }
-        return UICollectionReusableView()
 
+        return UICollectionReusableView()
     }
 
-    private func callingAlert(){
-        let alertController = UIAlertController(title: "Новая тема", message: "Напишите название новой темы", preferredStyle: .alert)
-        
+    private func callingAlert() {
+        let alertController = UIAlertController(title: "Новая тема",
+                                                message: "Напишите название новой темы",
+                                                preferredStyle: .alert)
         alertController.addTextField()
-        
-        let okAction = UIAlertAction(title: "Ok", style: .default) { (UIAlertAction) in
+
+        let okAction = UIAlertAction(title: "Ok",
+                                     style: .default) { _ in
             print("save")
-            
-            if alertController.textFields != nil && alertController.textFields![0].text != nil {
-                Storage.shared.themes.append(String(alertController.textFields![0].text!))     }
+
+            if alertController.textFields != nil
+                && alertController.textFields![0].text != nil {
+                Storage.shared.themes.append(String(alertController.textFields![0].text!))
+            }
             self.audioCollection.reloadData()
         }
-        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel) { (UIAlertAction) in
+
+        let cancelAction = UIAlertAction(title: "Отмена",
+                                         style: .cancel) { _ in
             print("cancel")
         }
-        
+
         alertController.addAction(okAction)
         alertController.addAction(cancelAction)
-        
-        
-        self.present(alertController, animated: true , completion: nil)
-        
+
+        present(alertController, animated: true)
     }
-    
 }
--- AudioNotes/AudioNotes/WorkerAudioFile.swift
+++ AudioNotes/AudioNotes/WorkerAudioFile.swift
@@ -10,23 +10,21 @@ import UIKit
 import AVFoundation
 
 class WorkerAudioFile {
-    
     var soundRecorder : AVAudioRecorder!
     var soundPlayer : AVAudioPlayer!
     var timer = Timer()
     var time = 0
     var settingAudioViewController = SettingAudioViewController()
     var fileName : String = "audioFile.m4a"
-    
 
     // Функция сохраняющая файл в директорию
     func getDocumetnsDirector() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        
+
         return paths[0]
     }
-    
-    func setupRecorder(viewController: UIViewController ){
+
+    func setupRecorder(viewController: UIViewController ) {
         Storage.shared.numberOfRecords += 1
         let audioFileName = getDocumetnsDirector().appendingPathComponent("Records\(Storage.shared.numberOfRecords).m4a")
         let recordSetting = [AVFormatIDKey : kAudioFormatAppleLossless,
@@ -34,28 +32,27 @@ class WorkerAudioFile {
                              AVEncoderBitRateKey : 320000,
                              AVNumberOfChannelsKey: 2,
                              AVSampleRateKey : 44100.2] as [String : Any]
-        
+
         do {
             soundRecorder = try AVAudioRecorder(url: audioFileName, settings: recordSetting)
             soundRecorder.delegate = viewController as? AVAudioRecorderDelegate
             soundRecorder.prepareToRecord()  //метод подготовки записи: создает файл и готовится к записи
-        }
-        catch{
+        } catch {
             print(error)
         }
     }
-    
+
     func setupPlayer(viewController: UIViewController) {
-        
         let audioFileName = getDocumetnsDirector().appendingPathComponent("Records\(Storage.shared.numberOfRecords).m4a")
+
         print(audioFileName)
+
         do {
             soundPlayer = try AVAudioPlayer(contentsOf: audioFileName) //какой воспроизводим файл
-            soundPlayer.delegate = viewController as? AVAudioPlayerDelegate 
+            soundPlayer.delegate = viewController as? AVAudioPlayerDelegate
             soundPlayer.prepareToPlay() //функция воспроизведения звука
             soundPlayer.volume = 1
-        }
-        catch {
+        } catch {
             print(error)
         }
     }
